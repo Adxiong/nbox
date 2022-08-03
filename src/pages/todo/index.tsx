aafd21dd59b5ba1aebdeda4a9e724ba699519528
adxiong <260245435@qq.com>
2022-08-04 00:49:03 +0800

    add todo tools

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,15 +1,18 @@
+import { Calendar } from 'antd';
+
 /*
  * @Description:
  * @version:
  * @Author: Adxiong
  * @Date: 2022-07-29 23:05:06
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-07-29 23:14:31
+ * @LastEditTime: 2022-08-04 00:40:54
  */
 const Todo = () => {
   return (
     <div>
       <span>这是todo页面</span>
+      <Calendar></Calendar>
     </div>
   );
 };
--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -1,15 +1,29 @@
+import { Card, Space } from 'antd';
+
 /*
  * @Description:
  * @version:
  * @Author: Adxiong
  * @Date: 2022-07-29 23:05:46
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-07-29 23:14:56
+ * @LastEditTime: 2022-08-04 00:46:51
  */
 const Tools = () => {
   return (
     <div>
       <span>这是工具页</span>
+      <Space wrap>
+        {new Array(100).fill(0).map((item) => {
+          return (
+            <Card
+              size="default"
+              style={{ width: 350, height: 250 }}
+              title="小工具"
+              extra={<a>详情</a>}
+            ></Card>
+          );
+        })}
+      </Space>
     </div>
   );
 };
