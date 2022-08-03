d5c350f8a327b87d2b32406fcccfe74e98a18758
adxiong <260245435@qq.com>
2022-08-04 00:19:55 +0800

    card add

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,10 +4,11 @@
  * @Author: Adxiong
  * @Date: 2022-07-30 23:56:21
  * @LastEditors: Adxiong
- * @LastEditTime: 2022-07-31 00:25:08
+ * @LastEditTime: 2022-08-04 00:18:31
  */
 
-import { Avatar, Button } from 'antd';
+import { Avatar, Button, Divider, Form, Input, Modal, Space } from 'antd';
+import { useState } from 'react';
 import './style/index.less';
 interface cardItemProps {
   src: string;
@@ -24,19 +25,43 @@ const CardItem = (props: cardItemProps) => {
   );
 };
 
-const CardHeader = () => {
+const CardHeader = (props: { onClick: () => void }) => {
   return (
     <div className="card-header">
       <span>xxx 分组共（xx）个内容</span>
-      <Button>新建收藏</Button>
+      <Button onClick={props.onClick}>新建收藏</Button>
     </div>
   );
 };
 
 const Card = () => {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [modalOnceLoading, setModalOnceLoading] = useState<boolean>(false);
+  const [modalLoading, setModalLoading] = useState<boolean>(false);
+
+  const handleAddCard = () => {
+    setModalVisible(true);
+  };
+  const submitAdd = () => {
+    // request methods
+    setModalOnceLoading(true);
+    setTimeout(() => {
+      setModalOnceLoading(false);
+      setModalVisible(false);
+    }, 2000);
+  };
+  const submitAddAgain = () => {
+    setModalLoading(true);
+    setTimeout(() => {
+      setModalLoading(false);
+    }, 2000);
+  };
+  const cancelAdd = () => {
+    setModalVisible(false);
+  };
   return (
     <div>
-      <CardHeader></CardHeader>
+      <CardHeader onClick={handleAddCard}></CardHeader>
 
       <div className="card">
         {new Array(100).fill(0).map((item) => {
@@ -47,6 +72,31 @@ const Card = () => {
           return <CardItem {...data}></CardItem>;
         })}
       </div>
+      <Modal
+        visible={modalVisible}
+        title="新建收藏"
+        centered
+        confirmLoading={modalLoading}
+        onCancel={cancelAdd}
+        footer={[
+          <Button onClick={cancelAdd}>取消</Button>,
+          <Button loading={modalOnceLoading} onClick={submitAdd}>
+            添加
+          </Button>,
+          <Button loading={modalLoading} onClick={submitAddAgain}>
+            再次添加
+          </Button>,
+        ]}
+      >
+        <Form>
+          <Form.Item label="名称">
+            <Input></Input>
+          </Form.Item>
+          <Form.Item label="地址">
+            <Input></Input>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
